03e7533c71fd7afd2c81f881a28b4fff24a55c86
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-04-11 20:10:57 -0500

    Cree nuevo launcher de aplicación

--- src/main/java/com/saulmmbp/just_another_hotel_alura/App.java
+++ src/main/java/com/saulmmbp/just_another_hotel_alura/App.java
@@ -1,13 +1,11 @@
 package com.saulmmbp.just_another_hotel_alura;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.awt.EventQueue;
+
+import com.saulmmbp.just_another_hotel_alura.view.HotelAluraGui;
+
+public class App {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(HotelAluraGui::new);
+	}
 }
